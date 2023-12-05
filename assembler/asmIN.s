ce367fa544e2f59d3ab71bc826f312dcf3ff855f
ItsCOMMANDer <111245538+ItsCOMMANDer@users.noreply.github.com>
2023-12-05 20:22:09 +0100

    kinda-ish working
    assembler works, besides labels and skipping the first line (for some reason)

---
+++ assembler/asmIN.s
@@ -0,0 +1,2 @@
+PUSH PC
+POP PC
--- assembler/ccas.c
+++ assembler/ccas.c
@@ -18,16 +18,16 @@ int main(int argc, char** argv) {
     }
 
     // Count number of lines
-    long lines = 1;
+    long amountOfLines = 1;
     char readCharacter;
     while ((readCharacter = fgetc(source_file)) != EOF) {
         if (readCharacter == '\n') {
-            lines++;
+            amountOfLines++;
         }
     }
 
     // Counts numbers of characters per line
-    int *characterAmount = (int*)calloc(lines, sizeof(int));
+    int *characterAmount = (int*)calloc(amountOfLines, sizeof(int));
     if (characterAmount == NULL) {
         perror("Memory allocation failed");
         fclose(source_file);
@@ -45,7 +45,7 @@ int main(int argc, char** argv) {
     }
 
     // Cpoies code into String array (2d char array)
-    char** code = (char**)malloc(lines * sizeof(char*));
+    char** code = (char**)malloc(amountOfLines * sizeof(char*));
 
     if(code == NULL) {
         perror("Memory allocation for code array Failed ");
@@ -56,7 +56,7 @@ int main(int argc, char** argv) {
     int charIndex = 0;
     int readChar;
     fseek(source_file, 0, SEEK_SET);
-    for(int i = 0; i < lines; i++) {
+    for(int i = 0; i < amountOfLines; i++) {
         code[i] = (char*)calloc(characterAmount[i] + 1, sizeof(char));
 
         if(code[i] == NULL) {
@@ -77,17 +77,20 @@ int main(int argc, char** argv) {
         code[i][characterAmount[i] + 1] = 0;
     }
 
+    fclose(source_file);
+
     
     int amountOfLabels = 0;    
-    for(int i = 0; i < lines; i++) {
+    for(int i = 0; i < amountOfLines; i++) {
         if(code[i][strlen(code[i]) - 1] == ':') amountOfLabels++;
     }
     
     char** label = calloc(amountOfLabels, sizeof(char*));
     uint16_t* labelAddresses = calloc(amountOfLabels, sizeof(uint16_t));
     uint16_t* labelLine = calloc(amountOfLabels, sizeof(uint16_t));
+
     int labelIndex = 0;
-    for(int i = 0; i < lines; i++) {
+    for(int i = 0; i < amountOfLines; i++) {
         if(code[i][strlen(code[i]) - 1] == ':') {
             label[labelIndex] = calloc(strlen(code[i]), sizeof(char));
             for(int j = 0; j < strlen(code[i]) - 1; j++) {
@@ -100,18 +103,72 @@ int main(int argc, char** argv) {
         }
     }
 
-    uint16_t *machine_code = calloc(lines - amountOfLabels, sizeof(uint16_t));
+    uint16_t *machine_code = calloc(amountOfLines - amountOfLabels, sizeof(uint16_t));
     if(machine_code == NULL) goto cleanup;
     int instructionIndex = 0;
     
     uint16_t args[3] = {0};
 
     asmParamsResult_t instruction;
+    instructionToken_t instruction_tok;
 
     int currentLabelIndex = 0;
 
-    //for(i)
-    //TODO: continue
+    for(int lineIndex = 0; lineIndex < amountOfLines; lineIndex++) {
+        if(lineIndex == labelLine[currentLabelIndex]) {
+            currentLabelIndex++;
+            continue;
+        }
+
+        instruction = getParametersFromAsembly(code[lineIndex]);
+        instruction_tok = getOpcode(instruction.params[0]);
+        printf("Instruction token = %d\ninstruction: %s\n", instruction_tok, instruction.params[0]);
+
+        if(instruction_tok == NAI) {
+            printf("Instruction \"%s\" doesnt exist.\n", instruction.params[0]);
+            goto cleanup;
+            return -1;
+        }
+        
+        for(int paramIndex = 0; paramIndex < instruction.amountOfParams; paramIndex++) {
+            switch(instructionPattern[instruction_tok][paramIndex]) {
+                case INSTRUCTION_TOKEN_REGISTER:
+                    if(getRegisterIndex(instruction.params[paramIndex + 1]) != NAR) {
+                        printf("Register at instruction %d at arg %d validated.\n", lineIndex, paramIndex);
+                        args[paramIndex] = getRegisterIndex(instruction.params[paramIndex + 1]);
+                    }
+                    else {printf("Register at instruction %d at arg %d not validated, because register doesnt exists\n", lineIndex, paramIndex); return -1;}
+                    break;
+
+                case INSTRUCTION_TOKEN_IMMIDIETE:
+                    if(isNumber(instruction.params[paramIndex + 1])) {
+                        if(atoi(instruction.params[paramIndex + 1]) <= 0b11111111) {
+                            printf("Immidiete at instruction %d at arg %d validated.\n", lineIndex, paramIndex);
+                            args[paramIndex] = (uint16_t)atoi(instruction.params[paramIndex + 1]);
+                        } else {printf("Immidiete at instruction %d at arg %d not validated, because number is to big (exeeds 8bit intiger limit).\n", lineIndex, paramIndex); return -1;}
+                    } else {printf("Immidiete at instruction %d at arg %d not validated, because arguments isnt a number.\n", lineIndex, paramIndex); return -1;}
+                    break;
+
+                case INSTRUCTION_TOKEN_ADDRESS:
+                    if(isNumber(instruction.params[paramIndex + 1])) {
+                        if(atoi(instruction.params[paramIndex + 1]) <= 0b11111111111) {
+                            printf("Offset at instruction %d at arg %d validated.", lineIndex, paramIndex);
+                            args[paramIndex] = atoi(instruction.params[paramIndex + 1]);
+                        } else {printf("Offset at instruction %d at arg %d not validated, because number is to big (exeeds 8bit intiger limit).\n", lineIndex, paramIndex); return -1;}
+                    } else {printf("Offset at instruction %d at arg %d not validated, because arguments isnt a number.\n", lineIndex, paramIndex); return -1;}
+                    break;
+            }
+        }
+
+        machine_code[instructionIndex] = assembleInstructions[(int)instruction_tok](args[0], args[1], args[2]);
+        printf("Finished instruction binary: 0x%X\n", machine_code[instructionIndex]);
+        instructionIndex++;
+
+        for(int index = 0; index < instruction.amountOfParams; index++) {
+            free(instruction.params[index]);
+        }
+        free(instruction.params);
+    }
 
 
 
@@ -122,24 +179,24 @@ int main(int argc, char** argv) {
 
     FILE *output_file = fopen(output_file_name, "wb");
 
-    fwrite(machine_code, sizeof(uint16_t), lines - amountOfLabels, output_file);
+    fwrite(machine_code, sizeof(uint16_t), amountOfLines - amountOfLabels, output_file);
 
     fclose(output_file);
 
     // clean up
     cleanup:
+    free(machine_code);
+
     for(int i = 0; i < amountOfLabels; i++) {
         free(label[i]);
     }
     free(label);
     free(labelAddresses);
 
-    for (int i = 0; i < lines; i++) {
+    for (int i = 0; i < amountOfLines; i++) {
         free(code[i]);
     }
-
     free(code);
     free(characterAmount);
-    fclose(source_file);
     return 0;
 }
