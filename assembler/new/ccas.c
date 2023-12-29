36c8ce74c3d97b3fd8f00beb13e889861a767027
ItsCOMMANDer <111245538+ItsCOMMANDer@users.noreply.github.com>
2023-12-29 15:43:32 +0100

    Update ccas.c

--- assembler/new/ccas.c
+++ assembler/new/ccas.c
@@ -4,6 +4,49 @@
 #include <string.h>
 #include <stdbool.h>
 
+
+const char instructions[][] = {
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+}
+
+
+
+
+
+
+
 int main() {
     printf("Hello, World!\n");
     return 42;
